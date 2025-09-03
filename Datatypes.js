6ecbfa59094c9ebcd48a4969b3aecc7b3bf4d59c
faizan-523 <faizan.7.ist@gmail.com>
2025-09-03 16:02:16 +0500

    Data types in javascript

---
+++ Datatypes.js
@@ -0,0 +1,25 @@
+let id = 23
+let Name ="Faizan"
+let isStudent = true
+let marks = 98.5
+let phoneNumber
+
+console.log(typeof 23)
+console.log(typeof Name)
+console.log(typeof isStudent)
+console.log(typeof marks)
+console.log(typeof phoneNumber) 
+
+/*
+Data Types in JavaScript:
+1. Number: Represents both integer and floating-point numbers
+2. String: Represents a sequence of characters.
+3. Boolean: Represents a logical entity and can have two values: true or false.
+4. Undefined: Represents a variable that has been declared but not assigned a value.
+5. Null: Represents the intentional absence of any object value.
+6. Object: Represents a collection of properties and methods.
+7. Symbol: Represents a unique identifier.
+8. BigInt: Represents integers with arbitrary precision, allowing for the representation of very large numbers
+beyond the safe integer limit of the Number type.
+
+*/
