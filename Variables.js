8edd7a753116bda453f89840a7d218c0a6638034
faizan-523 <faizan.7.ist@gmail.com>
2025-09-03 12:49:52 +0500

    Variables in Javascript

---
+++ Variables.js
@@ -0,0 +1,13 @@
+const id= 10
+let Name= "Faizan"
+var age= 20
+city = "Islamabad"
+
+console.table({id, Name, age, city})
+
+//id=20 not allowed because id is constant
+
+Name= "Ali"
+age= 25
+city= "Lahore"
+console.table({id, Name, age, city})
