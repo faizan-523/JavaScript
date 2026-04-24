859c2059676fff537eba80875e6208d524f94d4a
faizan-523 <faizan.7.ist@gmail.com>
2026-04-24 14:43:02 +0500

    Comparisons in JavaScript

---
+++ Comparison.js
@@ -0,0 +1,19 @@
+// Comparisons
+console.log(5 > 23);
+console.log(5 < 23);
+console.log(5 >= 5);
+console.log(5 <= 5);
+console.log(5 == "5");
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 5);
+console.log(undefined > 5);
+console.log(undefined < 5);
+
+// === also checks for data type and value both
+
+console.log(23 === "23"); // not equal because of different data types
+
