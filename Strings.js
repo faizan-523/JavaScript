ec5848c0a2f46b526fe409013140467f84cc3a76
faizan-523 <faizan.7.ist@gmail.com>
2026-04-25 14:46:08 +0500

    Strings

---
+++ Strings.js
@@ -0,0 +1,24 @@
+name = "Faizan"
+age = 20
+
+console.log(name + age)
+console.log(`My Name is ${name} and my age is ${age}`);
+
+console.log("My Name is " + name + " and my age is " + age);
+
+console.log(name.length);
+
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+
+console.log(name.slice(0, 3));
+console.log(name.replace("Faizan", "Ali"));
+
+console.log(name.includes("Faizan"));
+console.log(name.includes("Ali"));
+
+console.log(name.indexOf("a"));
+
+console.log(name.charAt(0));
+console.log(name.split("a"));
+
